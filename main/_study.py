db24e1528ce9c59db2311615cb162aecedbd076c
ElliottQian <752004979@qq.com>
2017-08-27 17:31:37 +0800

    加了很多print

--- main/_study.py
+++ main/_study.py
@@ -2,25 +2,37 @@ import main.autodiff as ad
 import numpy as np
 
 
-def __identity():
-
+def _mul_two_vars():
     x2 = ad.Variable(name="x2")
-    y = x2
+    x3 = ad.Variable(name="x3")
+    y = x2 * x3
 
-    grad_x2, = ad.gradients(y, [x2])
+    grad_x2, grad_x3 = ad.gradients(y, [x2, x3])
 
-    executor = ad.Executor([y, grad_x2])
+    executor = ad.Executor([y, grad_x2, grad_x3])
     x2_val = 2 * np.ones(3)
-    print(x2_val)
-    y_val, grad_x2_val = executor.run(feed_dict={x2: x2_val})
-
-    print(y_val, grad_x2_val)
+    x3_val = 3 * np.ones(3)
+    y_val, grad_x2_val, grad_x3_val = executor.run(feed_dict={x2: x2_val, x3: x3_val})
 
     assert isinstance(y, ad.Node)
-    assert np.array_equal(y_val, x2_val)
-    assert np.array_equal(grad_x2_val, np.ones_like(x2_val))
+    assert np.array_equal(y_val, x2_val * x3_val)
+    assert np.array_equal(grad_x2_val, x3_val)
+    assert np.array_equal(grad_x3_val, x2_val)
+
+
+def x2():
+    x2 = ad.Variable(name="x2")
+    x3 = ad.Variable(name="x3")
+    x4 = x2 * x3
+    y = x4 * x4
+    grad_list = ad.gradients(y, [x2, x3, x4])
+    for g in grad_list:
+        print("-------21------")
+        print(g)
 
 
 if __name__ == "__main__":
-    __identity()
+    #_mul_two_vars()
+    print("-------------")
+    x2()
     pass
